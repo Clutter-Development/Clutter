da114460104d689d1755685bddfa270c0e731f26
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-02-27 15:35:07 +0300

    ping command

--- modules/misc/invite.py
+++ modules/misc/invite.py
@@ -13,14 +13,14 @@ class Invite(commands.Cog):
     @commands.bot_has_permissions(send_messages=True, embed_links=True, read_message_history=True)
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def _invite(self, ctx):
-        await ctx.channel.trigger_typing()
         await ctx.reply(
             embed=embed.info(
                 ctx.guild.id,
                 "Invite me",
                 f"[Bot invite]({bot_info['invite']})\n[Support server]({bot_info['discord']})",
             )
-        )
+            ,
+            mention_author=False)
 
 
 def setup(bot):
---
+++ modules/misc/ping.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+from utils.init import embed
+
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @commands.command(name="ping")
+    @commands.guild_only()
+    @commands.bot_has_permissions(send_messages=True, embed_links=True, read_message_history=True)
+    @commands.cooldown(1, 2, commands.BucketType.user)
+    async def _ping(self, ctx):
+        await ctx.reply(embed=embed.info(ctx.guild.id, f"Pong! `{round(self.bot.latency * 1000)}ms`"),
+                        mention_author=False)
+
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
