10f0f7c5373da417c502b67e7fdd631f3011710c
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-04-14 17:41:21 +0300

    remove unneeded import

--- bot/utils/database/manager.py
+++ bot/utils/database/manager.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, List, Tuple, Union
 
 from motor import motor_asyncio
 
