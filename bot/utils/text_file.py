498aed5efa030dadde48efe0f30ec374b12e56ff
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-04-14 16:43:17 +0300

    docs docs docs docs docs docs docs

--- bot/utils/text_file.py
+++ bot/utils/text_file.py
@@ -6,4 +6,14 @@ __all__ = ("text_file",)
 
 
 def text_file(text: str, file_name: str, /, *, spoiler: bool = False) -> discord.File:
+    """Creates a discord.File object from a string.
+
+    Args:
+        text (str): The text to be converted to a discord.File object.
+        file_name (str): The name of the file.
+        spoiler (bool, optional): Whether or not the file is a spoiler. Defaults to False.
+    
+    Returns:
+        discord.File: The discord.File object.
+    """
     return discord.File(StringIO(text), file_name, spoiler=spoiler)  # type: ignore
