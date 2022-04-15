187013f5e6ab3585ae480006a8ca5d4147b604d3
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-04-15 08:38:10 +0300

    add checks (might not work)

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,7 +1,7 @@
 import color
 
 from .checks import *
-from .embed import *
 from .database import *
+from .embed import *
 from .listify import *
 from .text_file import *
--- bot/utils/checks/check.py
+++ bot/utils/checks/check.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
+
 from typing import TYPE_CHECKING, Callable, Optional
 
 if TYPE_CHECKING:
+    from discord import Interaction
+    from discord import app_commands as app
+
     from ..database import CachedMongoManager
-    from discord import Interaction, app_commands as app
 
 __all__ = ("CommandChecks",)
 
 
 class CommandChecks:
-
     def __init__(self, db: CachedMongoManager, /):
         self._db = db
 
@@ -17,6 +19,7 @@ class CommandChecks:
     def bot_admin_only() -> Callable:
         async def predicate(inter: Interaction, /) -> bool:
             return inter.user.id in inter.client.admin_ids  # type: ignore
+
         return app.check(predicate)
 
     @staticmethod
@@ -25,6 +28,5 @@ class CommandChecks:
             if inter.user.id in inter.client.admin_ids:  # type: ignore
                 return None
             return app.Cooldown(rate, per)
+
         return app.checks.dynamic_cooldown(predicate)
-            
-            
