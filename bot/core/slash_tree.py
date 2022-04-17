f2fe9bcb19d4a7b45eccf39c7c14d97967970fe1
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-04-17 21:34:30 +0300

    start working on slash_tree

--- bot/core/slash_tree.py
+++ bot/core/slash_tree.py
@@ -6,12 +6,22 @@ class ClutterCommandTree(app.CommandTree):
     def interaction_check(self, inter: discord.Interaction, /) -> bool:
         if self.client.in_development and inter.user.id not in self.client.admin_ids:
             await inter.response.defer(ephemeral=True, thinking=True)
-            await inter.response.send_message(ephemeral=True, embed=self.client.embed.error("You can't use this command", "The bot is currently in development mode. Only bot admins can use this command."))
+            await inter.response.send_message(
+                ephemeral=True,
+                embed=self.client.embed.error(
+                    "You can't use this command",
+                    "The bot is currently in development mode. Only bot admins can use this command.",
+                ),
+            )
             return False
         elif self.client.db.get(f"users.{inter.user.id}.blacklisted", default=False):
             await inter.response.defer(ephemeral=True, thinking=True)
-            await inter.response.send_message(ephemeral=True, embed=self.client.embed.error("You can't use this command", "You are blacklisted from the bot.\nIf you think this is a mistake, please a bot admin to unblacklist you."))
+            await inter.response.send_message(
+                ephemeral=True,
+                embed=self.client.embed.error(
+                    "You can't use this command",
+                    "You are blacklisted from the bot.\nIf you think this is a mistake, please a bot admin to unblacklist you.",
+                ),
+            )
             return False
         return True
-
-
