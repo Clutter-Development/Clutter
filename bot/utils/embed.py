86144104f30afe94bf203169ce8cae6bfde6f692
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-04-26 23:45:00 +0300

    ignore

--- bot/utils/embed.py
+++ bot/utils/embed.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING  # , Callable, overload
+from typing import TYPE_CHECKING, Callable, Any  # , overload
 
 from discord import Embed
 
