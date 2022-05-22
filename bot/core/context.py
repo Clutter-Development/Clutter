f3472ab14f7ca4fa83962312b9a80e437a3a230a
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-05-22 23:01:50 +0300

    add ping and invite

--- bot/core/context.py
+++ bot/core/context.py
@@ -4,20 +4,22 @@ from discord.ext import commands
 
 class ClutterContext(commands.Context):
     async def reply_embed(
-        self, asset_type: str, title: str, /, description: str | None = None
+        self, asset_type: str, title: str | None = None, /, description: str | None = None, *, mention: bool = False, view: discord.ui.View | None = None
     ) -> discord.Message:
 
         """Replies an embed to the context.
 
         Args:
             asset_type (str): The embed asset type to reply with.
-            title (str): The title of the embed.
+            title (str | None): The title of the embed.
             description (str | None, optional): The description of the embed. Defaults to None.
+            mention (bool): Wheter or not to mention the author when replying. Defaults to False.
+            view (discord.ui.View | None): The View to send with the message. Defaults to None
 
         Returns:
             discord.Message: The replied message.
         """
-        return await self.reply(embed=self.bot.embed(asset_type, title, description))
+        return await self.reply(embed=self.bot.embed(asset_type, title, description=description), mention_author=mention, view=view)
 
     async def ok(self, value: bool, /) -> None:
         """Reacts to the context message depending on the value.
@@ -29,13 +31,13 @@ class ClutterContext(commands.Context):
         await self.message.add_reaction(emojis["SUCCESS" if value else "ERROR"])
 
     async def maybe_dm_embed(
-        self, asset_type: str, title: str, description: str | None = None, /
+        self, asset_type: str, title: str | None = None, /, description: str | None = None
     ) -> tuple[discord.Message | None, bool]:
         """DMs the author of the context an embed.
 
         Args:
             asset_type (str): The embed asset type to DM with.
-            title (str): The title of the embed.
+            title (str | None): The title of the embed.
             description (str | None, optional): The description of the embed. Defaults to None.
 
         Returns:
@@ -43,7 +45,7 @@ class ClutterContext(commands.Context):
         """
         try:
             return (
-                await self.author.send(embed=self.bot.embed(asset_type, title, description)),
+                await self.author.send(embed=self.bot.embed(asset_type, title, description=description)),
                 True,
             )
         except (discord.Forbidden, discord.HTTPException):
--- bot/modules/misc.py
+++ bot/modules/misc.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
+
+import time
 from typing import TYPE_CHECKING
-import discord
+
+import discord.ui
 from discord.ext import commands
-from discord import app_commands as app
 
 if TYPE_CHECKING:
     from core.bot import Clutter
@@ -24,8 +26,28 @@ class Misc(commands.Cog, name="MODULES.MISCELLANIOUS.NAME", description="MODULES
 
         # help command is basically in this cog
         self._original_help_command = bot.help_command
-        bot.help_command = ClutterHelpCommand(verify_checks=False, brief="COMMANDS.HELP.BRIEF", help="COMMANDS.HELP.HELP")
+        bot.help_command = ClutterHelpCommand(verify_checks=False, brief="COMMANDS.HELP.BRIEF",
+                                              help="COMMANDS.HELP.HELP")
         bot.help_command.cog = self
 
     async def cog_unload(self) -> None:
         self.bot.help_command = self._original_help_command
+
+    @commands.command(brief="COMMANDS.PING.BRIEF", help="COMMANDS.PING.HELP")
+    @commands.bot_has_permissions(send_messages=True, read_message_history=True)
+    async def ping(self, ctx: ClutterContext):
+        ts = time.monotonic()
+        message = await ctx.reply("** **", mention_author=False)
+        await message.edit(embed=self.bot.embed.info(self.bot.i18n(ctx, "COMMANDS.PING.RESPONSE.TITLE"),
+                                                     self.bot.i18n(ctx, "COMMANDS.PING.RESPONSE.BODY").format(
+                                                         ws_ping=self.bot.latency * 1000,
+                                                         message_ping=int((time.monotonic() - ts) * 1000), db_ping=0)))  # TODO: database ping
+
+    @commands.command(brief="COMMANDS.INVITE.BRIEF", help="COMMANDS.INVITE.HELP")
+    @commands.bot_has_permissions(send_messages=True, read_message_history=True)
+    async def invite(self, ctx: ClutterContext):
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(label=self.bot.i18n("COMMANDS.INVITE.BUTTONS.BOT_INVITE"), url=self.bot.invite_url))
+        view.add_item(
+            discord.ui.Button(label=self.bot.i18n("COMMANDS.INVITE.BUTTONS.DISCORD_INVITE"), url=self.bot.discord_invite))
+        await ctx.reply_embed("info", view=view)
