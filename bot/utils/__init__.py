86e7e39aadfcc193c8a6565dc0eb9f73bdbe6899
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-05-22 17:28:54 +0300

    merge listify and text_file into one file

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -2,5 +2,4 @@ from . import color, errors
 from .database import *
 from .embed import *
 from .i18n import *
-from .listify import *
-from .text_file import *
+from .misc import *
--- bot/utils/misc.py
+++ bot/utils/misc.py
@@ -1,6 +1,23 @@
+import io
 import re
 
-__all__ = ("listify",)
+import discord
+
+__all__ = ("text_file", "listify")
+
+
+def text_file(text: str, file_name: str, /, *, spoiler: bool = False) -> discord.File:
+    """Creates a discord.File object from a string.
+
+    Args:
+        text (str): The text to be converted to a discord.File object.
+        file_name (str): The name of the file.
+        spoiler (bool, optional): Whether or not the file is a spoiler. Defaults to False.
+
+    Returns:
+        discord.File: The discord.File object.
+    """
+    return discord.File(io.BytesIO(bytes(text, "utf-8")), file_name, spoiler=spoiler)
 
 
 def listify(title: str, description: str, /) -> str:
