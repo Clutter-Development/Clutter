3b8800ec9cea04761162e3323d407c28a2ac2515
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-05-22 23:02:39 +0300

    add aliases

--- bot/core/context.py
+++ bot/core/context.py
@@ -4,7 +4,14 @@ from discord.ext import commands
 
 class ClutterContext(commands.Context):
     async def reply_embed(
-        self, asset_type: str, title: str | None = None, /, description: str | None = None, *, mention: bool = False, view: discord.ui.View | None = None
+        self,
+        asset_type: str,
+        title: str | None = None,
+        /,
+        description: str | None = None,
+        *,
+        mention: bool = False,
+        view: discord.ui.View | None = None,
     ) -> discord.Message:
 
         """Replies an embed to the context.
@@ -19,7 +26,11 @@ class ClutterContext(commands.Context):
         Returns:
             discord.Message: The replied message.
         """
-        return await self.reply(embed=self.bot.embed(asset_type, title, description=description), mention_author=mention, view=view)
+        return await self.reply(
+            embed=self.bot.embed(asset_type, title, description=description),
+            mention_author=mention,
+            view=view,
+        )
 
     async def ok(self, value: bool, /) -> None:
         """Reacts to the context message depending on the value.
@@ -45,7 +56,9 @@ class ClutterContext(commands.Context):
         """
         try:
             return (
-                await self.author.send(embed=self.bot.embed(asset_type, title, description=description)),
+                await self.author.send(
+                    embed=self.bot.embed(asset_type, title, description=description)
+                ),
                 True,
             )
         except (discord.Forbidden, discord.HTTPException):
--- bot/modules/misc.py
+++ bot/modules/misc.py
@@ -20,14 +20,17 @@ class ClutterHelpCommand(commands.HelpCommand):
         pass
 
 
-class Misc(commands.Cog, name="MODULES.MISCELLANIOUS.NAME", description="MODULES.MISCELLANIOUS.DESCRIPTION"):
+class Misc(
+    commands.Cog, name="MODULES.MISCELLANIOUS.NAME", description="MODULES.MISCELLANIOUS.DESCRIPTION"
+):
     def __init__(self, bot: Clutter):
         self.bot = bot
 
         # help command is basically in this cog
         self._original_help_command = bot.help_command
-        bot.help_command = ClutterHelpCommand(verify_checks=False, brief="COMMANDS.HELP.BRIEF",
-                                              help="COMMANDS.HELP.HELP")
+        bot.help_command = ClutterHelpCommand(
+            verify_checks=False, brief="COMMANDS.HELP.BRIEF", help="COMMANDS.HELP.HELP"
+        )
         bot.help_command.cog = self
 
     async def cog_unload(self) -> None:
@@ -38,16 +41,32 @@ class Misc(commands.Cog, name="MODULES.MISCELLANIOUS.NAME", description="MODULES
     async def ping(self, ctx: ClutterContext):
         ts = time.monotonic()
         message = await ctx.reply("** **", mention_author=False)
-        await message.edit(embed=self.bot.embed.info(self.bot.i18n(ctx, "COMMANDS.PING.RESPONSE.TITLE"),
-                                                     self.bot.i18n(ctx, "COMMANDS.PING.RESPONSE.BODY").format(
-                                                         ws_ping=self.bot.latency * 1000,
-                                                         message_ping=int((time.monotonic() - ts) * 1000), db_ping=0)))  # TODO: database ping
+        await message.edit(
+            embed=self.bot.embed.info(
+                self.bot.i18n(ctx, "COMMANDS.PING.RESPONSE.TITLE"),
+                self.bot.i18n(ctx, "COMMANDS.PING.RESPONSE.BODY").format(
+                    ws_ping=self.bot.latency * 1000,
+                    message_ping=int((time.monotonic() - ts) * 1000),
+                    db_ping=0,
+                ),
+            )
+        )  # TODO: database ping
 
-    @commands.command(aliases=["support"], brief="COMMANDS.INVITE.BRIEF", help="COMMANDS.INVITE.HELP")
+    @commands.command(
+        aliases=["support"], brief="COMMANDS.INVITE.BRIEF", help="COMMANDS.INVITE.HELP"
+    )
     @commands.bot_has_permissions(send_messages=True, read_message_history=True)
     async def invite(self, ctx: ClutterContext):
         view = discord.ui.View()
-        view.add_item(discord.ui.Button(label=self.bot.i18n("COMMANDS.INVITE.BUTTONS.BOT_INVITE"), url=self.bot.invite_url))
         view.add_item(
-            discord.ui.Button(label=self.bot.i18n("COMMANDS.INVITE.BUTTONS.DISCORD_INVITE"), url=self.bot.discord_invite))
+            discord.ui.Button(
+                label=self.bot.i18n("COMMANDS.INVITE.BUTTONS.BOT_INVITE"), url=self.bot.invite_url
+            )
+        )
+        view.add_item(
+            discord.ui.Button(
+                label=self.bot.i18n("COMMANDS.INVITE.BUTTONS.DISCORD_INVITE"),
+                url=self.bot.discord_invite,
+            )
+        )
         await ctx.reply_embed("info", view=view)
