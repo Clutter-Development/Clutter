ebd61ba969a628bdbb43905da3c567646d3124da
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-05-24 23:05:22 +0300

    fix 3

--- bot/utils/database/manager.py
+++ bot/utils/database/manager.py
@@ -158,5 +158,5 @@ class MongoManager:
     async def __aenter__(self):
         return self
 
-    async def __aexit__(self):
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self._client.close()
