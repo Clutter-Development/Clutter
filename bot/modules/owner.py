85075e65cd30e6b74fa078d127e1683c8a5648d2
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-06 17:57:21 +0300

    remove unused import

--- bot/modules/owner.py
+++ bot/modules/owner.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Literal, Optional
 
 import discord
-from discord import app_commands as app
 from discord.ext import commands
 
 if TYPE_CHECKING:
