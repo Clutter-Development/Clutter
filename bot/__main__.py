e4881899e156c383292beb5cec1dea478d3c2b69
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-06 10:23:50 +0300

    add jsk envs

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,13 @@
 import sentry_sdk
 import uvloop
+import os
 from core.bot import bot
 
 uvloop.install()
 
 sentry_sdk.init(bot._config["SENTRY_KEY"], traces_sample_rate=1.0)
+
+for key in ["HIDE", "NO_UNDERSCORE"]:
+    os.environ[f"JISHAKU_{key}"] = "True"
 
 bot.run()
