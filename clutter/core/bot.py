bfafae6b32fe841e25d0dee2225d664592b19539
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-14 23:22:18 +0300

    more

--- clutter/core/bot.py
+++ clutter/core/bot.py
@@ -1,17 +1,20 @@
 import asyncio
-
+import itertools
 import aiohttp
+import traceback
+import os
 import discord
 import time
 import pathlib
 from discord.ext import commands
-from ..utils import color, db, embed, i18n
+from ..utils import color, db, embed, i18n, format_as_list
 
 ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
 
 
 class ClutterBot(commands.AutoShardedBot):
     invite_url: str
+    start_log: str
     start_time: float
 
     def __init__(self, config: dict, session: aiohttp.ClientSession, /) -> None:
@@ -114,18 +117,143 @@ class ClutterBot(commands.AutoShardedBot):
     async def get_context(self, message: discord.Message, /) -> ClutterContext:
         return await super().get_context(message, cls=ClutterContext)
 
-    # Start the bot.
+    # Blacklist checking.
 
-    def run(self) -> None:
-        async def runner() -> None:
-            async with self:
-                await self.load_extensions()
-                await self.start(self.token)
+    async def guild_check(self, guild: discord.Guild, /) -> bool:
+        if await self.db.get(f"guilds.{guild.id}.blacklisted"):
+            await asyncio.gather(
+                guild.leave(),
+                self.db.set(f"users.{guild.owner_id}.blacklisted", True),
+            )
+            return False
+        elif await self.db.get(f"users.{guild.owner_id}.blacklisted"):
+            await asyncio.gather(
+                guild.leave(),
+                self.db.set(f"guilds.{guild.id}.blacklisted", True),
+            )
+            return False
+        return True  # True means the guild is safe.
 
-        try:
-            asyncio.run(runner())
-        except KeyboardInterrupt:
-            pass
+    async def check_all_guilds(self) -> None:
+        await asyncio.gather(*(self.guild_check(guild) for guild in self.guilds))
+
+    async def on_guild_join(self, guild: discord.Guild) -> None:
+        await self.guild_check(guild)
+
+    async def blacklist_user(self, user: int | discord.Object, /) -> bool:
+        user_id = user if isinstance(user, int) else user.id
+
+        if await self.db.get(f"users.{user_id}.blacklisted"):
+            return False
+
+        await self.db.set(f"users.{user_id}.blacklisted", True)
+        return True
+
+    async def unblacklist_user(self, user: int | discord.Object, /) -> bool:
+        user_id = user if isinstance(user, int) else user.id
+
+        if not await self.db.get(f"users.{user_id}.blacklisted"):
+            return False
+
+        await self.db.set(f"users.{user_id}.blacklisted", False)
+        return True
+
+    # Gets a member object.
+
+    async def getch_member(
+        self, guild: discord.Guild, user_id: int, /
+    ) -> discord.Member | None:
+        if (member := guild.get_member(user_id)) is not None:
+            return member
+
+        if self.get_shard(guild.shard_id).is_ws_ratelimited():
+            try:
+                return await guild.fetch_member(user_id)
+            except discord.HTTPException:
+                return None
+
+        if members := await guild.query_members(
+            limit=1, user_ids=[user_id], cache=True
+        ):
+            return members[0]
+
+    # Loads all the cogs.
+
+    async def load_extensions(self) -> None:
+        loaded = []
+        failed = {}
+        for fn in itertools.chain(
+            map(
+                lambda fp: ".".join(fp.parts)[:-3],
+                (ROOT_DIR / "modules").relative_to(os.getcwd()).rglob("*.py"),
+            ),
+            ["jishaku"],
+        ):
+            try:
+                await self.load_extension(fn)
+                loaded.append(fn.rsplit(".", 1)[-1])
+            except (
+                commands.ExtensionFailed,
+                commands.NoEntryPointError,
+                commands.ExtensionNotFound,
+            ):
+                failed[fn.rsplit(".", 1)[-1]] = traceback.format_exc()
+
+        log = []
+        if loaded:
+            log.append(
+                color.green(
+                    format_as_list(
+                        f"Successfully loaded {len(loaded)} modules",
+                        "\n".join(loaded),
+                    )
+                )
+            )
+        log.extend(
+            color.red(
+                format_as_list(f"Failed to load {color.bold(name)}", error)
+            )
+            for name, error in failed.items()
+        )
+
+        self.start_log = "\n\n".join(log)
+
+    async def on_ready(self) -> None:
+        print(
+            "\n\n".join(
+                [
+                    self.start_log,
+                    color.cyan(
+                        format_as_list(
+                            "Discord Info",
+                            f"{color.bold('Version:')} {discord.__version__}",
+                        )
+                    ),
+                    color.magenta(
+                        format_as_list(
+                            "Bot Info",
+                            "\n".join(
+                                [
+                                    f"{color.bold('User:')} {self.user}",
+                                    f"{color.bold('ID:')} {self.user.id}",
+                                    f"{color.bold('Total Guilds:')} {len(self.guilds)}",
+                                    f"{color.bold('Total Users:')} {len(self.users)}",
+                                    f"{color.bold('Total Shards:')} {self.shard_count}",
+                                ]
+                            ),
+                        )
+                    ),
+                    color.yellow(f"Running on Clutter v{self.version}"),
+                ]
+            )
+        )
+
+    # Starts the bot.
+
+    async def start(self) -> None:
+        async with self:
+            await self.load_extensions()
+            await self.start(self.token)
 
 
 
