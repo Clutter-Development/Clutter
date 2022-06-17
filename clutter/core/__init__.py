cb26411b43c4837f427074c718cc9d6b7ea6579c
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-17 18:21:47 +0300

    add errors

--- clutter/core/__init__.py
+++ clutter/core/__init__.py
@@ -1,4 +1,5 @@
 from .bot import *
 from .command_tree import *
 from .context import *
+from .errors import *
 from .interaction import *
--- clutter/core/errors.py
+++ clutter/core/errors.py
@@ -1,12 +1,10 @@
 from discord import app_commands as app
 from discord.ext import commands
 
+__all__ = ("ClutterError", "UserIsBlacklisted", "UserHasBeenBlacklisted")
+
 
 class ClutterError(app.AppCommandError, commands.CommandError):
-    pass
-
-
-class BotInMaintenance(ClutterError):
     pass
 
 
