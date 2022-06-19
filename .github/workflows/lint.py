581334212e8ea794f0f4159a0549dec1ca9dd67f
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-19 19:47:11 +0300

    nvm

--- .github/workflows/lint.py
+++ .github/workflows/lint.py
@@ -5,8 +5,8 @@ from re import match
 
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
-for fp in ROOT_DIR.rglob("*"):
-    if not fp.is_file() or not (content := fp.read_text()):
+for fp in ROOT_DIR.rglob("__init__.py"):
+    if not (content := fp.read_text()):
         continue
 
     old_content = content[:]
@@ -14,15 +14,10 @@ for fp in ROOT_DIR.rglob("*"):
     while content[0] == "\n":
         content = content[1:]
 
-    if str(fp) == "__init__.py":
-        content = content.removeprefix("from __future__ import annotations")
-
-    if not content.endswith("\n"):
-        content += f"{content}\n"
+    content = content.removeprefix("from __future__ import annotations")
 
     while content[0] == "\n":
         content = content[1:]
 
     if old_content != content:
-
         fp.write_text(content)
